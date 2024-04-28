422833733339ec3eaff485a449d61e3c8d707466
Murodjoni Sharifzoda <murodjoni@Murodjonis-MacBook-Pro.local>
2024-04-28 15:07:29 -0600

    Added AppData and initiated launchApp method inside AppFactory

--- src/main/driver/AppFactory.java
+++ src/main/driver/AppFactory.java
@@ -10,7 +10,7 @@ public class AppFactory {
 
     static AppiumDriver driver;
 
-    public static void android_launchApp() throws MalformedURLException {
+    private static void android_launchApp() throws MalformedURLException {
         options.setDeviceName("emulator-5554")
         .setPlatformVersion("14.0")
         .setAppPackage("com.saucelabs.mydemoapp.rn")
@@ -21,9 +21,18 @@ public class AppFactory {
         System.out.println("AndroidDriver is set");
     }
 
-    public static void ios_launchApp(XCUITestOptions options) throws MalformedURLException {
+    private static void ios_launchApp(XCUITestOptions options) throws MalformedURLException {
         driver = new IOSDriver(new URL("http://127.0.0.1:4723/"), options);
         AppDriver.setDriver(driver);
         System.out.println("IOS_Driver is set");
 }
+
+    public static void launchApp(){
+        if(AppData.platform.contains("android")){
+            AppFactory.android_launchApp();
+        }else if(AppData.platform.contains("ios")){
+            AppFactory.ios_launchApp(null);
+        }else
+        throw new SkipException("Enter a valid platform value android/ios");
+    }
 }
